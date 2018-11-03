affde44c1ef27caec6730ee27d443b33ac90ab25
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-11-03 17:34:50 -0400

    Update indexeddb.js

--- code/indexeddb.js
+++ code/indexeddb.js
@@ -1,1 +1,46 @@
+//Deal with prefixes
+window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction
 
+
+window.idb = {};
+
+(async function(){
+    
+    
+    let _db = new Promise(function(resolve,reject){
+        
+        let store = indexedDB.open("storage", 1);   
+        
+        store.onupgradeneeded = function(){
+            let db = event.target.result;
+            db.createObjectStore("data", { keyPath: "key" });
+        }
+        
+        store.onsuccess = function(){
+            resolve(db.result)
+        }
+        
+    })    
+    
+
+    
+    
+    
+    window.idb.prep = async function(){await _db}
+    
+    window.idb.set = async function(key, value) {
+        let trans = _db.transaction("storage", "readwrite")
+        let store = transaction.objectStore("data");
+        
+        let request = objectStore.put(value, key);
+
+        
+        return request
+    }
+    
+    
+
+
+
+}());

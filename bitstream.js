24489045d1aea2dcc88cb6cf4a134c7b637b0b08
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-12-09 10:02:05 -0500

    Update bitstream.js

--- bitstream.js
+++ bitstream.js
@@ -1,25 +1,58 @@
-let bitstream = function(bytes) {
+let bitstream = function(buffer) {
+   
+  this.buffer = new Uint8Array(buffer);
   
-  this.buffer = new Uint8Array(bytes)
-  
-  
-  this.bytenum = 0
-  this.bitpos = 7
-  
-  this.bytesize = bytes
-  
+  this.bytenum = 0;
+  this.bitpos = 7;
+    
   this.write = function(bit) {
       
     //Set the bit to zero if not already
-    //Note: See if you can use a XOR instead to reduce this to a single operation
-    this.buffer[this.bytenum] -= this.buffer[this.bytenum] & (1<<this.bitpos)
-    this.buffer[this.bytenum] += bit<<this.bitpos
+    //Note: See if you can use a XOR instead to reduce this to a single operation. I don't think so.
+
+    this.buffer[this.bytenum] -= this.buffer[this.bytenum] & (1<<this.bitpos);
+    this.buffer[this.bytenum] += bit<<this.bitpos;
    
-    if (this.bitpos-- < 0) {
-      this.bytenum++
-      this.bitpos = 7
+    this.bitpos--
+    if (this.bitpos < 0) {
+      this.bytenum++;
+      this.bitpos = 7;
     }
+        
+    return;
+  }
+  
+  
+  this.read = function() {
+    
+    let res = this.buffer[this.bytenum] & (1<<this.bitpos);
+    
+    this.bitpos--
+    if (this.bitpos < 0) {
+      this.bytenum++;
+      this.bitpos = 7;
+    }
+    
+    if (res) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  
+  this.writepos = function(bit, bytenum, bitpos) {
+    
+    this.buffer[bytenum] -= this.buffer[bytenun] & (1<<bitpos);
+    this.buffer[bytenum] += bit<<bitpos;
     
     return;
   }
+  
+  
+  this.readpos = function(bytenum, bitpos) {
+    if (this.buffer[bytenum] & (1<<bitpos)) {
+      return 1;
+    }
+    return 0;
+  }  
 }
